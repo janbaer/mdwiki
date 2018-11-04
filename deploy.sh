345420bb41ffe8d755fdabf6b1385e4523d1e235
Jan Baer <jan@janbaer.de>
2018-11-04 11:35:29 +0100

    Create deployment script to deploy MDWiki to the Github gh-page

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+VERSION=$1
+
+if [ -z "$VERSION" ]; then
+  echo "Please pass a version as command arg"
+  exit 1
+fi
+
+echo "Deploying new version $VERSION to gh-pages branch"
+
+cd deploy
+
+git rm --cached -r .
+
+cp ../dist/*.* .
+cp ../CNAME .
+
+if [ -f "./styles/*.*.map" ]; then
+  rm ./styles/*.*.map
+fi
+
+git add -u && git add .
+
+git commit -m "Version ${VERSION}"
+
+git push -u origin gh-pages
+
