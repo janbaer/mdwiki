76d59116b482bae106fe447f7ae1d41a2476b5f9
Jan Baer <jan@janbaer.de>
2018-11-05 06:05:22 +0100

    Add manifest.json to support PWA

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,12 @@ cd deploy
 
 git rm -r .
 
+mkdir images
+
 cp ../dist/*.* .
 cp ../CNAME .
+cp ../src/images/favicon.ico ./images/
+cp ../src/images/wiki.png ./images/
 
 if [ -f "./styles/*.*.map" ]; then
   rm ./styles/*.*.map
