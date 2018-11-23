9d2ebb8ae062a9b7e584da1c1a86c7b89333814f
Jan Baer <jan@janbaer.de>
2018-11-23 06:00:41 +0100

    Version 0.12

--- uikit-icons.9ee4fb5a.js
+++ uikit-icons.9ee4fb5a.js
@@ -301,7 +301,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "32867" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36865" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
