e2ab4c40bb3daf319a76bacbfa72e1ac551eae6c
Jan Baer <jan@janbaer.de>
2018-12-01 18:21:06 +0100

    Sidebar: Show links to all Groups on top and scroll to group on click

--- src/app/pages/home/components/sidebar.js
+++ src/app/pages/home/components/sidebar.js
@@ -8,10 +8,17 @@ export default class Sidebar extends Component {
     super(props);
     this.renderPageEntry = this.renderPageEntry.bind(this);
     this.renderGroup = this.renderGroup.bind(this);
+    this.renderGroupLink = this.renderGroupLink.bind(this);
+    this.scrollTo = this.scrollTo.bind(this);
   }
 
   gotoPage(page) {
     this.props.onClick(page.name);
+  }
+
+  scrollTo(e) {
+    e.preventDefault();
+    document.querySelector(`#group${e.target.innerText}`).scrollIntoView({ behavior: 'smooth' });
   }
 
   renderPageEntry(page) {
@@ -25,7 +32,7 @@ export default class Sidebar extends Component {
   renderGroup(group) {
     return (
       <div>
-        <h4 class="Sidebar-pageGroupHeader">{group.letter}</h4>
+        <h4 id={`group${group.letter}`} class="Sidebar-pageGroupHeader">{group.letter}</h4>
 
         <ul class="Sidebar-list">
           {group.pages.map(this.renderPageEntry)}
@@ -34,11 +41,19 @@ export default class Sidebar extends Component {
     );
   }
 
+  renderGroupLink(group) {
+    return (
+      <a href="#" onClick={this.scrollTo}>{group.letter}</a>
+    );
+  }
+
   render({ pages }, state) {
-    console.log('pages', pages);
     const groups = groupPages(pages);
     return (
       <div class="Sidebar-listContainer">
+        <div class="Sidebar-groupLinkContainer">
+          {groups.map(this.renderGroupLink)}
+        </div>
         {groups.map(this.renderGroup)}
       </div>
     );
