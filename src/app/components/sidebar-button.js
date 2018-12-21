000d6b864740694231c609c1af821cdbf6776cb4
Jan Baer <jan@janbaer.de>
2018-12-21 10:17:35 +0100

    Replace Sidemenu icons with Hamburger icon

--- src/app/components/sidebar-button.js
+++ src/app/components/sidebar-button.js
@@ -1,15 +1,13 @@
 import { h } from 'preact';
 
-import LeftArrow from './../../images/left-arrow.svg';
-import RightArrow from './../../images/right-arrow.svg';
+import HamburgerMenuSvg from './../../images/hamburger-menu.svg';
 
 import './sidebar-button.less';
 
 const SidebarButton = ({ showSidebar, onClick }) => {
   return (
     <button class="SidebarButton-button" title="Add" onClick={onClick}>
-      { showSidebar && <LeftArrow /> }
-      { !showSidebar && <RightArrow /> }
+      <HamburgerMenuSvg />
     </button>
   );
 };
