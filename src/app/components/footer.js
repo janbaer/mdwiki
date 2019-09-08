f16c00bdbf3543aa7d7c21702425e3bc95856cf4
Jan Baer <jan@janbaer.de>
2019-09-08 18:36:20 +0200

    v42: Show always current year in Copyright

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -4,7 +4,7 @@ import './footer.less';
 
 const Footer = ({ appVersion }) => (
   <footer>
-    <strong>MDWiki 3.{ appVersion }</strong>&nbsp;-&nbsp;Copyright 2018 by Jan Baer
+    <strong>MDWiki 3.{ appVersion }</strong>&nbsp;-&nbsp;Copyright { new Date().getFullYear() } by Jan Baer
   </footer>
 );
 
